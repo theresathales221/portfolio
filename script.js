61ad3c01382aa37827d2e7b580f7e2037ca9b47e
theresathales221 <duskwriter1@gmail.com>
2026-01-18 23:36:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let dragged = null
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key)=>{
+    key.addEventListener("dragstart", (event) =>{
+    dragged = event.target
+    })
+}
+    
+    
+);
+
+const target = document.getElementById("droptarget");
+target.addEventListener("dragover",(event)=>{
+    event.preventDefault();
+});
+
+
+target.addEventListener("drop", (event) => {
+  event.preventDefault();
+    dragged.remove();
+
+  target.textContent = dragged.textContent;
+  
+
+  const content = document.getElementById(`${dragged.id}-content`);
+///${}と``を組み合わせると要素を探せる？
+  if(content) {
+    content.style.display = "block";
+  }
+
+  if(dragged.id === "key4"){
+    const key5flag = document.getElementById("key5");
+    if(key5flag){
+        key5flag.style.display = "block";
+    }
+  }
+    
+ 
+});
+
+///もっと色々仕込みたかった...。
